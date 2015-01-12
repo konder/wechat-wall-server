a6692dace6062af11229a3a6cff1049c0e6771c2
zhangnan <zhangnan@lngtop.com>
2015-01-13 02:12:00 +0800

    remove spam field

--- ctrler/shake.js
+++ ctrler/shake.js
@@ -8,13 +8,18 @@ exports.getUser = function (_userid) {
         // 在没有加载完，使用默认值填充
         users[_userid] = {'userid': _userid};
 
-        http.get(config.wechatapi + '/wechat/user/' + _userid, function (res) {
+        http.get(config.wechatapi + '/user/' + _userid, function (res) {
             var json = '';
             res.on('data', function (chunk) {
                 json += chunk;
             }).on('end', function () {
                 var _u = json && JSON.parse(json);
-                _u && (users[_userid] = _u);
+                _u && (users[_userid] = {
+                    userid: _u.userid,
+                    name: _u.name,
+                    gender: _u.gender,
+                    avatar: _u.avatar
+                });
             })
         }).on('error', function (e) {
             console.log(e);
